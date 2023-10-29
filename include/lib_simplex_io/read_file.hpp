f8b23e36147ac781b14161f024f7050fde99c923
AVAtarMod <Respawner289@gmail.com>
2023-10-29 21:39:48 +0300

    Add lib

--- include/lib_simplex_io/read_file.hpp
+++ include/lib_simplex_io/read_file.hpp
@@ -1,28 +1,43 @@
 #ifndef LIB_SIMPLEX_IO_READ_FILE_HPP
 #define LIB_SIMPLEX_IO_READ_FILE_HPP
-#include <vector>
 #include <sstream>
+#include <vector>
 
-struct ParsedFunction
-{
-   std::vector<std::pair<std::string, float>> function;
-   SimplexMethodSolver::MinMaxType function_type;
-   bool success = true;
-};
+namespace lib_simplex_io {
+   enum MinMaxType
+   {
+      MIN,
+      MAX
+   };
+   enum ConstraintType
+   {
+      NONE,
+      LESSER_OR_EQUAL,
+      GREATER_OR_EQUAL,
+      EQUAL,
+   };
 
-struct ParsedConstraint
-{
-   typedef std::vector<float> SingleConstraint;
-   SingleConstraint constraint;
-   SimplexMethodSolver::ConstraintType constraint_type =
-       SimplexMethodSolver::NONE;
-   bool success = true;
-   bool is_last = false;
-};
+   struct ParsedFunction
+   {
+      std::vector<std::pair<std::string, float>> function;
+      MinMaxType function_type;
+      bool success = true;
+   };
 
-ParsedFunction readFunction(std::istream &in,
-                            const size_t buffer_size = 255);
-ParsedConstraint readConstraint(std::istream &in,
-                                const size_t buffer_size = 255);
+   struct ParsedConstraint
+   {
+      typedef std::vector<float> SingleConstraint;
+      SingleConstraint constraint;
+      ConstraintType constraint_type = NONE;
+      bool success = true;
+      bool is_last = false;
+   };
+
+   ParsedFunction readFunction(std::istream& in,
+                               const size_t buffer_size = 255);
+   ParsedConstraint readConstraint(std::istream& in,
+                                   const size_t buffer_size = 255);
+
+} // namespace lib_simplex_io
 
 #endif // LIB_SIMPLEX_IO_READ_FILE_HPP
