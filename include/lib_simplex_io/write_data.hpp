3df6469081b20dd4bf167a63f46790d6083a659b
AVAtarMod <Respawner289@gmail.com>
2023-11-07 10:09:47 +0300

    [include] Implement write_data

--- include/lib_simplex_io/write_data.hpp
+++ include/lib_simplex_io/write_data.hpp
@@ -1,12 +1,21 @@
-#ifndef LIB_SIMPLEX_IO_WRITE_FILE_HPP
-#define LIB_SIMPLEX_IO_WRITE_FILE_HPP
+#ifndef LIB_SIMPLEX_IO_WRITE_DATA_HPP
+#define LIB_SIMPLEX_IO_WRITE_DATA_HPP
 #include <ostream>
 
 #include "types.hpp"
 namespace lib_simplex_io {
-   bool writeFunction(std::ostream& in, const FunctionBase& function);
-   bool writeConstraint(std::ostream& in, const ConstraintBase& constraint);
-   bool writeSimplexTableau(std::ostream& in, const SimplexTableau& tableau);
+   std::ostream& operator<<(std::ostream& out,
+                            const FunctionBase& function);
+   std::ostream& operator<<(std::ostream& out,
+                            const ConstraintBase& constraint);
+   std::ostream& operator<<(std::ostream& out,
+                            const SimplexTableau& tableau);
+   bool writeFunction(std::ostream& out,
+                      const FunctionBase& function);
+   bool writeConstraint(std::ostream& out,
+                        const ConstraintBase& constraint);
+   bool writeSimplexTableau(std::ostream& out,
+                            const SimplexTableau& tableau);
 } // namespace lib_simplex_io
 
-#endif // LIB_SIMPLEX_IO_WRITE_FILE_HPP
+#endif // LIB_SIMPLEX_IO_WRITE_DATA_HPP
