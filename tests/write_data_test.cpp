3092fb2bbeed0670080420c3e2c18f68fafaf0b1
AVAtarMod <Respawner289@gmail.com>
2023-11-08 19:09:27 +0300

    Add test project

---
+++ tests/write_data_test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+#include "lib_simplex_io/write_data.hpp"
+
+TEST(WriteData, test1){
+   using namespace lib_simplex_io;
+   SimplexTableau t;
+   t.basis_variables_indexes = { 1, 2 };
+   t.variable_count = 4;
+   t.function_count = 1;
+   t.table = { { 1, 2, 3 },
+               { 99999999.123456, 0.2, -4 },
+               { -1, -2, -4 } };
+   // std::cout << std::setprecision(1);
+   writeSimplexTableau(std::cout, t);
+}
